951982770b90a98b9e338960a89e17abd7b9d959
rachaen <rachaenlee@gmail.com>
2021-12-11 20:32:51 +0900

    저작권

---
+++ BOJ_JAVA/src/bronze5/P2914.java
@@ -0,0 +1,13 @@
+package bronze5;
+
+import java.util.Scanner;
+
+public class P2914 {
+	public static void main(String[] args) {
+		 Scanner sc = new Scanner(System.in);
+		 int a = sc.nextInt();	// ¼ö·ÏµÈ °îÀÇ °³¼ö
+		 int i = sc.nextInt();	// Æò±Õ°ª 
+		 
+		 System.out.println(a * (i-1) + 1);
+	}
+}
