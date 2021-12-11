67176090d500c226dca8fa2d869ef27404475f3c
rachaen <rachaenlee@gmail.com>
2021-12-11 20:33:21 +0900

    웰컴

---
+++ BOJ_JAVA/src/bronze5/P5337.java
@@ -0,0 +1,11 @@
+package bronze5;
+
+public class P5337 {
+	public static void main(String[] args) {
+		System.out.println("       _.-;;-._");
+		System.out.println("'-..-'|   ||   |");
+		System.out.println("'-..-'|_.-;;-._|");
+		System.out.println("'-..-'|   ||   |");
+		System.out.println("'-..-'|_.-''-._|");
+	}
+}
