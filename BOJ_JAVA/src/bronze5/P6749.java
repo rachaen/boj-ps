64ee3a21385bfb174d8fa3cce28ff2f3798a11fa
rachaen <rachaenlee@gmail.com>
2021-12-15 22:39:38 +0900

    Next in line

---
+++ BOJ_JAVA/src/bronze5/P6749.java
@@ -0,0 +1,12 @@
+package bronze5;
+
+import java.util.Scanner;
+
+public class P6749 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int youngest_child = sc.nextInt();
+		int middle_child = sc.nextInt(); 
+		System.out.println(2 * middle_child - youngest_child );
+	}
+}
