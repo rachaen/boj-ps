c6209ba659662303aa693defb37bb644e834e6ad
rachaen <rachaenlee@gmail.com>
2021-12-16 09:58:38 +0900

    ??!

---
+++ BOJ_JAVA/src/bronze5/P10926.java
@@ -0,0 +1,11 @@
+package bronze5;
+
+import java.util.Scanner;
+
+public class P10926 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String id = sc.next();
+		System.out.println(id + "??!");
+	}
+}
