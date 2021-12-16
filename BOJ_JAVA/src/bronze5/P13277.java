53681c91088d80108eaaf404ef0993396a312355
rachaen <rachaenlee@gmail.com>
2021-12-16 17:37:39 +0900

    큰 수 곱셈

---
+++ BOJ_JAVA/src/bronze5/P13277.java
@@ -0,0 +1,12 @@
+package bronze5;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class P13277 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		BigInteger first = sc.nextBigInteger();
+		System.out.println(first.multiply(sc.nextBigInteger()));
+	}
+}
