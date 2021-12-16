b65949e64eaaac0c5aad29e7defbe5d9e2d9453d
rachaen <rachaenlee@gmail.com>
2021-12-16 17:23:21 +0900

    한글 2

---
+++ BOJ_JAVA/src/bronze5/P11283.java
@@ -0,0 +1,12 @@
+package bronze5;
+
+import java.util.Scanner;
+
+public class P11283 {
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+		char result = s.charAt(0);
+		System.out.println((int)result - 44031);
+	}
+}
