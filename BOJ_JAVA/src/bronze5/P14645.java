fede27cd923f5b4c8e7be7e156f176b3df2433d7
rachaen <rachaenlee@gmail.com>
2021-12-16 17:37:48 +0900

    와이어스 부릉부릉

---
+++ BOJ_JAVA/src/bronze5/P14645.java
@@ -0,0 +1,11 @@
+package bronze5;
+
+import java.util.Scanner;
+
+public class P14645 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		sc.next();
+		System.out.println("ºñ¿ÍÀÌ");
+	}
+}
