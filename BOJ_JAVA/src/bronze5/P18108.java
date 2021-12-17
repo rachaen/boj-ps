198c140989e753f388e716b72d1218e3880d391d
rachaen <rachaenlee@gmail.com>
2021-12-18 01:03:34 +0900

    1998년생인 내가 태국에서는 2541년생?!

---
+++ BOJ_JAVA/src/bronze5/P18108.java
@@ -0,0 +1,11 @@
+package bronze5;
+
+import java.util.Scanner;
+
+public class P18108 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int year = sc.nextInt();
+		System.out.println(year-543);
+	}
+}
