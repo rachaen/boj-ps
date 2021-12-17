e41242be4236be88ff5b568cbeb258ad312817ff
rachaen <rachaenlee@gmail.com>
2021-12-17 21:44:50 +0900

    수학은 체육과목 입니다

---
+++ BOJ_JAVA/src/bronze5/P15894.java
@@ -0,0 +1,12 @@
+package bronze5;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class P15894 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		BigInteger n = sc.nextBigInteger();
+		System.out.println(n.multiply(BigInteger.valueOf(4)));
+	}
+}
