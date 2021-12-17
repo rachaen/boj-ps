03f68ab372f6963ed498cd4793a2c1e358f65f49
rachaen <rachaenlee@gmail.com>
2021-12-17 16:51:18 +0900

    Vera and Outfits

---
+++ BOJ_JAVA/src/bronze5/P15439.java
@@ -0,0 +1,11 @@
+package bronze5;
+
+import java.util.Scanner;
+
+public class P15439 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		System.out.println(n*(n-1));
+	}
+}
