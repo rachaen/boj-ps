a4306237e138476b4c178935353d96d428e4665c
rachaen <rachaenlee@gmail.com>
2021-12-17 22:05:19 +0900

    제리와 톰

---
+++ BOJ_JAVA/src/bronze5/P16430.java
@@ -0,0 +1,12 @@
+package bronze5;
+
+import java.util.Scanner;
+
+public class P16430 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int p = sc.nextInt();
+		int q = sc.nextInt();
+		System.out.printf("%d %d", q-p, q);
+	}
+}
