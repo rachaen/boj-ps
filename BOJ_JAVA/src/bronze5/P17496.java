46c467fedd2c4b135cd2d14e3ec69b66c0d25fc7
rachaen <rachaenlee@gmail.com>
2021-12-18 00:57:21 +0900

    스타후르츠

---
+++ BOJ_JAVA/src/bronze5/P17496.java
@@ -0,0 +1,14 @@
+package bronze5;
+
+import java.util.Scanner;
+
+public class P17496 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int t = sc.nextInt();
+		int c = sc.nextInt();
+		int p = sc.nextInt();
+		System.out.println(((n-1)/t) * c * p);
+	}
+}
