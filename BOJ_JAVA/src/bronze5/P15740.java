d8764258ef380104bcc4433c4706b2f0088af644
rachaen <rachaenlee@gmail.com>
2021-12-17 17:49:37 +0900

    A+B - 9

---
+++ BOJ_JAVA/src/bronze5/P15740.java
@@ -0,0 +1,13 @@
+package bronze5;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class P15740 {
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BigInteger first = sc.nextBigInteger();
+        BigInteger second = sc.nextBigInteger();
+        System.out.println(first.add(second));    
+	}
+}
