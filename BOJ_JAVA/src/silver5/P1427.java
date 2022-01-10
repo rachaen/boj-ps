9e518cdb5d4b812d67b5dc576dffafb3292e0575
rachaen <rachaenlee@gmail.com>
2022-01-10 13:26:30 +0900

    소트인사이드

---
+++ BOJ_JAVA/src/silver5/P1427.java
@@ -0,0 +1,21 @@
+package BOJ_JAVA.src.silver5;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class P1427 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        char[] arr = br.readLine().toCharArray();
+        Arrays.sort(arr);
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            bw.write(arr[i]);
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
