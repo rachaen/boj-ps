acdd801bcfc4f67d731f25b355402c8b48fe50fb
rachaen <rachaenlee@gmail.com>
2022-01-11 22:05:19 +0900

    단어의 개수

---
+++ BOJ_JAVA/src/bronze/P1152.java
@@ -0,0 +1,16 @@
+package BOJ_JAVA.src.bronze;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class P1152 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        bw.write(st.countTokens()+"\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
