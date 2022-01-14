bf223df0785f22b59bbbc8f94f067b79e377712f
rachaen <rachaenlee@gmail.com>
2022-01-14 11:22:19 +0900

    A+B - 5

---
+++ BOJ_JAVA/src/bronze3/P10952.java
@@ -0,0 +1,26 @@
+package BOJ_JAVA.src.bronze3;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class P10952 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        String str;
+
+        while( (str=br.readLine()) != null ){
+            st = new StringTokenizer(str," ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            if(a == 0 && b == 0) {
+                break;
+            }
+            bw.write((a+b) + "\n");
+        }
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
