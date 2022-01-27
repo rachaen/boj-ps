d7c9d69c9055ac051a1b762ccd05134b65529baa
rachaen <rachaenlee@gmail.com>
2022-01-27 23:59:11 +0900

    숫자의 개수

---
+++ BOJ_JAVA/src/bronze/P2577.java
@@ -0,0 +1,22 @@
+package BOJ_JAVA.src.bronze;
+
+import java.io.*;
+
+public class P2577 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int[] arr = new int[10];
+
+        int val = Integer.parseInt(br.readLine()) * Integer.parseInt(br.readLine()) * Integer.parseInt(br.readLine());
+
+        String str = String.valueOf(val);
+        for (int i = 0; i < str.length(); i++) {
+            arr[(str.charAt(i) - 48)]++;
+        }
+        for (int v : arr) {
+            bw.write(v+ "\n");
+        }
+
+    }
+}
