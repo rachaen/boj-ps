7a30fd5ea408982a6d298b6da01562760a951490
rachaen <rachaenlee@gmail.com>
2022-02-27 23:28:32 +0900

    달팽이는 올라가고 싶다

---
+++ BOJ_JAVA/src/bronze/P2869.java
@@ -0,0 +1,26 @@
+package BOJ_JAVA.src.bronze;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class P2869 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int V = Integer.parseInt(st.nextToken());
+
+        int day = (V - B) / (A - B);
+        if ((V - B) % (A - B) != 0)
+            day++;
+        bw.write(String.valueOf(day));
+
+        bw.flush();
+        bw.close();
+        br.close();
+
+    }
+}
